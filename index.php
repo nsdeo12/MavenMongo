9b537a6a4c616037e3ce9e8cc4c746e090059f74
nsdeo12 <nsdeo12@hotmail.com>
2016-06-14 14:00:22 +0530

    initial setup for maven and mongo with spark Web

--- index.php
+++ index.php
@@ -8,7 +8,25 @@
 
 <body>
 	<h1>LCA Visualisation</h1>
-
+    <p>
+        <label for="input" 
+                style="display: inline-block; width: 200px; text-align: right">
+                tree threshold
+        </label>	
+        <input type="range" min="0" max="600" step ="10" id="treeinput">
+        
+        <label for="input" 
+                style="display: inline-block; width: 200px; text-align: right">
+                mid point threshold
+        </label>
+        <input type="range" min="0" max="100" step ="1" id="input">
+    
+        <label for="input" 
+                style="display: inline-block; width: 200px; text-align: right">
+                End point threshold
+        </label>
+        <input type="range" min="0" max="100" step ="1" id="input2">
+    </p>
 	<!-- load the d3.js library -->	
 	<script src="d3.js"></script>
 	<script src="script.js"></script>
--- script.js
+++ script.js
@@ -46,8 +46,9 @@ var datasetP1End = [
   {name: "pastry",     value: 42},
   {name: "dosa",   value: 7},
   {name: "roti", value: 23},
-  {name: "curry",     value: 62}
+  {name: "curry",value: 62}
 ];
+
 // ************** Generate the tree diagram	 *****************
 var margin = {top: 20, right: 120, bottom: 20, left: 100},
 	width = 1200 - margin.right - margin.left,
@@ -71,55 +72,17 @@ d3.select("svg").append("g")
 	.attr("height", height + margin.top + margin.bottom)
     .attr("rx", 25);
 
-//tree drawing starts
-var tree = d3.layout.tree()
-	.size([tree_height,tree_width]);
-
-var diagonal = d3.svg.diagonal()
-	.projection(function(d) { return [d.y, d.x]; });
-
-
-var treeg = d3.select("svg").append("g")
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-    .attr("id", "main_tree");
-
-// load the external data
-
-d3.json("./treedata.json", function(error, json) {
-  if (error) throw error;
-
-  var nodes = tree.nodes(json),
-      links = tree.links(nodes);
-
-  var link = treeg.selectAll("path.link")
-      .data(links)
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", diagonal);
-
-  var node = treeg.selectAll("g.node")
-      .data(nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-
-  node.append("circle")
-      .attr("r", 10).style('stroke','#000');
-
-  node.append("text")
-      .attr("dx", function(d) { return d.children ? -12 : 12; })
-      .attr("dy", 3)
-      .attr("text-anchor", function(d) { return d.children ? "end" : "start"; })
-      .text(function(d) { return d.name; });
-});
-
+makeTree(40);
 // tree drawing ends
-
 
 //Generation of Level 1 graph
 var level1graph_container = d3.select("svg").append("g")
     .attr("transform", "translate(" + (margin.left+tree_width+100) + "," + margin.top + ")")
     .attr("id", "level1graph_container");
+
+d3.select("svg").append("text")
+    .attr("transform", "translate(" + (margin.left+tree_width+100+100) + ",18)")
+    .html("Mid Point")
 
 level1graph_container.append("rect")
     .attr("width", barGraphRectWidth)
@@ -137,11 +100,83 @@ level2graph_container.append("rect")
     .attr("height", barGraphRectHeight)
     .attr("class", "bargraphRectangle")
     .attr("rx", 25);
+d3.select("svg").append("text")
+    .attr("transform", "translate(" + (margin.left+tree_width+barGraphRectWidth+150+100) + ",18)")
+    .html("End Point")
+    
+    
+makeGraph(datasetP1,"#level1graph_container",20);
+makeGraph(datasetP1End,"#level2graph_container",10);   
 
-makeGraph(datasetP1,"#level1graph_container");
-makeGraph(datasetP1End,"#level2graph_container");   
-/* ********** function to make graph *********** */
-function makeGraph(dataSet,graphContainer) {
+/* ********** function to make tree *********** */
+function makeTree(threshold)
+{
+	var tree = d3.layout.tree()
+		.size([tree_height,tree_width]);
+
+	var diagonal = d3.svg.diagonal()
+		.projection(function(d) { return [d.y, d.x]; });
+
+
+	var treeg = d3.select("svg").append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+		.attr("id", "main_tree");
+
+	// load the external data
+
+	d3.json("./treedata.json", function(error, json) {
+	  if (error) throw error;
+	  console.log(json);
+	  for(i = 0; i<json.children.length; i++)
+	  {
+	  	console.log(json.children[i].name);
+	  	for(j=0; j<json.children[i].children.length; j++)
+	  	{
+	  		console.log(" "+json.children[i].children[j].name);
+	  		if(json.children[i].children[j].children) {
+		  		for(k=0; k<json.children[i].children[j].children.length; k++)
+		  		{
+		  			if(json.children[i].children[j].children[k].value > threshold)
+		  				console.log("  "+json.children[i].children[j].children[k].name+" "+json.children[i].children[j].children[k].value);
+		  			else {
+			  				json.children[i].children[j].children.splice(k,1);
+			  				k--;
+			  				console.log("    THRESHOLD");
+		  				 }
+		  		}
+	  		}
+	  	}
+	  }
+	  
+	  var nodes = tree.nodes(json),
+		  links = tree.links(nodes);
+
+	  var link = treeg.selectAll("path.link")
+		  .data(links)
+		.enter().append("path")
+		  .attr("class", "link")
+		  .attr("d", diagonal);
+
+	  var node = treeg.selectAll("g.node")
+		  .data(nodes)
+		.enter().append("g")
+		  .attr("class", "node")
+		  .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+	  node.append("circle")
+		  .attr("r", 10).style('stroke','#000');
+
+	  node.append("text")
+		  .attr("dx", function(d) { return d.children ? -12 : 12; })
+		  .attr("dy", 3)
+		  .attr("text-anchor", function(d) { return d.children ? "end" : "start"; })
+		  .text(function(d) { return d.name; });
+	});
+}
+/* ********** function to make tree ends******* */
+
+/* ********** function to make graph ********** */
+function makeGraph(dataSet,graphContainer,threshold) {
     dataSet.sort(function(a,b) {return b.value-a.value});        
     var xscale = d3.scale.linear()
         .domain([0, d3.max(dataSet, function(d){return d.value})])
@@ -152,9 +187,8 @@ function makeGraph(dataSet,graphContainer) {
         .orient("top");
         
     var yscale = d3.scale.ordinal()
-        .domain(dataSet.map(function(d) { return d.name; }))
-        .rangeBands([0,barGraphHeight],.1,.1);
-
+        .domain(dataSet.map(function(d) { if(d.value>threshold) return d.name; }))
+        .rangeRoundBands([0,barGraphHeight],0.1,0);
     var yAxis = d3.svg.axis()
         .scale(yscale)
         .orient("left");
@@ -162,20 +196,25 @@ function makeGraph(dataSet,graphContainer) {
     var chart = d3.select(graphContainer)
         .append("g")
         .attr("transform", "translate(50,30)")
+        .attr("id","grapha")
         .attr("width", barGraphWidth);
 
     var bar = chart.selectAll("rect")
-        .data(dataSet)
+        .data(dataSet.filter(function(d){return d.value>threshold}))
     .enter().append("rect")
-        .attr("width", function(d) { return xscale(d.value); })
+        .attr("width", 0)
         .attr("height", yscale.rangeBand())
         .attr('x',0)
         .attr('y',function(d,i){
             return yscale(d.name);
         })
-        .attr("rx",3)
         .attr("class", "barRectangles");
-
+    bar.transition().duration(800)
+        .attr("width", function(d) { return xscale(d.value); })
+        .delay(function(d,i){
+            return i*30;
+        })
+        .ease('back');
 
     chart.append("g")
         .attr("class", "x axis")
@@ -186,3 +225,22 @@ function makeGraph(dataSet,graphContainer) {
         .attr("transform", "translate(0," + 0 + ")")
         .call(yAxis);
 }
+
+/* ********** updating the graphs ************* */
+d3.select("#level1graph_containera").remove();
+d3.select("#input").on("input", function() {
+  console.log(this.value);
+  d3.select("#grapha").remove();
+  makeGraph(datasetP1,"#level1graph_container",this.value);
+});
+/* ********** updating grpahs ends ************ */
+
+/* ********** updating the tree  ************** */
+
+d3.select("#treeinput").on("input", function() {
+	console.log(this.value);
+	d3.select("#main_tree").remove();
+	makeTree(this.value);
+})
+
+/* ********** updating tree ends ************** */
